a2755d3687296f9f553aede425620c9db9ef11c8
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2021-09-17 22:03:55 +0300

    jsx-a11y works, firefox gives two keyboard warnings

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/strict",
+    "plugin:react/jsx-runtime",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {},
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PropTypes from "prop-types";
 
-function App() {
+function App({ subject }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Make it a11ble!</h1>
+        <img src={logo} className="App-logo" alt="atom and electrons" />
       </header>
+      <p role="main">Hello, {subject}!</p>
     </div>
   );
 }
-
+App.propTypes = {
+  subject: PropTypes.string.isRequired,
+};
 export default App;
