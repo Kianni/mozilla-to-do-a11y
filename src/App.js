93b36ac89f8adb4a97ba27d01ed30cb5f2ea63b7
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2021-09-19 14:25:51 +0300

    starting point with css not working properly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,100 @@
-import logo from "./logo.svg";
-import "./App.css";
 import PropTypes from "prop-types";
 
-function App({ subject }) {
+function App(props) {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Make it a11ble!</h1>
-        <img src={logo} className="App-logo" alt="atom and electrons" />
-      </header>
-      <p role="main">Hello, {subject}!</p>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+            <input
+              type="text"
+              id="new-todo-input"
+              className="input input__lg"
+              name="text"
+              autoComplete="off"
+            />
+          </label>
+        </h2>
+
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <label className="todo-label" htmlFor="todo-0">
+              <input id="todo-0" type="checkbox" defaultChecked={true} />
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <label className="todo-label" htmlFor="todo-1">
+              Sleep
+              <input id="todo-1" type="checkbox" />
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <label className="todo-label" htmlFor="todo-2">
+              Repeat
+              <input id="todo-2" type="checkbox" />
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Repeat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
